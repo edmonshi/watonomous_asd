f03783181c79d330d9784dd6d7c5f5646260f7fc
Edmon <edmonshi2005@gmail.com>
2025-09-20 23:58:50 -0400

    Initial commit

---
+++ src/robot/costmap/src/costmap_node.cpp
@@ -0,0 +1,14 @@
+#include <chrono>
+#include <memory>
+
+#include "costmap_node.hpp"
+
+CostmapNode::CostmapNode() : Node("costmap"), costmap_(robot::CostmapCore(this->get_logger())) {}
+
+int main(int argc, char ** argv)
+{
+  rclcpp::init(argc, argv);
+  rclcpp::spin(std::make_shared<CostmapNode>());
+  rclcpp::shutdown();
+  return 0;
+}
